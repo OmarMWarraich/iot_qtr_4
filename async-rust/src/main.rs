a18067da6b958ffad1069cd27e6bf7c30e69c783
Omar Mohammad <omar@Remo.omar>
2021-09-12 16:55:55 -0400

    Async/.await

--- async-rust/src/main.rs
+++ async-rust/src/main.rs
@@ -1,7 +1,7 @@
 use std::thread;
 use std::time::Duration;
 use std::time::Instant;
-// use async_std::task;
+use async_std::task;
 
 // fn upload_media(){
 //     println!("Uploading media starts");
@@ -53,26 +53,35 @@ thread::sleep(Duration::from_millis(2000));
 println!("Finished cleaning room");
 }
 //Async returns future
+
 async fn make_tea(){
 // takes 20 minutes
 println!("Started making tea");
-thread::sleep(Duration::from_millis(2000));
+task::sleep(Duration::from_millis(1000)).await;
 println!("Finished making tea");
 }
 
-fn wash_dishes(){
+async fn wash_dishes(){
 // takes 20 minutes
 println!("Started washing dishes");
-thread::sleep(Duration::from_millis(2000));
+task::sleep(Duration::from_millis(1000)).await;
 println!("Finished washing dishes");
 }
 
-fn main(){
-    let start = Instant::now();
-    clean_room();
-    let future = make_tea();
-    block_on(future);
-    wash_dishes();
-    let elapsed = start.elapsed();
+async fn do_work_efficiently(){
+    task::spawn(make_tea()); //async
+    
+    clean_room();   // 2sec
+    
+    let result = wash_dishes().await;  //2 sec
+    make_tea();
+}
+
+fn main() {
+    let start = Instant::now();//00:02
+    block_on(do_work_efficiently());
+
+
+    let elapsed = start.elapsed(); // 00:56 
     println!("Time taken : {:?}", elapsed);
 }
