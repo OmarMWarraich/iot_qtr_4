e178eef03583ac4acf94a3bf28f547d407c86efe
Omar Mohammad <omar@Remo.omar>
2021-09-12 16:05:39 -0400

    Add dependency futures and simple code

--- async-rust/src/main.rs
+++ async-rust/src/main.rs
@@ -1,36 +1,73 @@
 use std::thread;
 use std::time::Duration;
-use std::time::Instant;
+// use std::time::Instant;
 
-fn upload_media(){
-    println!("Uploading media starts");
-    thread::sleep(Duration::from_millis(500));
-    println!("Uploading media ends");
+// fn upload_media(){
+//     println!("Uploading media starts");
+//     thread::sleep(Duration::from_millis(500));
+//     println!("Uploading media ends");
+// }
+
+// fn related_post_details(){
+//     println!("Posting other details starts");
+//     thread::sleep(Duration::from_millis(50));
+//     println!("Posting other details ends");
+// }
+
+// fn add_post(){
+//     // let thread_1 = thread::spawn(|| {
+        
+//         upload_media();
+        
+//     // });
+
+//     // let thread_2 = thread::spawn(|| {
+        
+//         related_post_details();
+//     // });
+//     // thread_1.join().expect("Thread one panicked")
+// }
+
+// fn main() {
+//     let start = Instant::now();
+//     add_post();
+//     let elapsed = start.elapsed();
+//     println!("Time elapsed {:?}", elapsed);
+// }
+
+// use futures::executor::block_on;
+
+// async fn hello_world(){
+//     println!("test");
+// }
+
+// fn main(){
+//     block_on(hello_world());
+// }
+
+fn clean_room(){
+// takes 20 minutes
+println!("Started cleaning room");
+thread::sleep(Duration::from_millis(2000));
+println!("Finished cleaning room");
 }
 
-fn related_post_details(){
-    println!("Posting other details starts");
-    thread::sleep(Duration::from_millis(50));
-    println!("Posting other details ends");
+fn make_tea(){
+// takes 20 minutes
+println!("Started making tea");
+thread::sleep(Duration::from_millis(2000));
+println!("Finished making tea");
 }
 
-fn add_post(){
-    // let thread_1 = thread::spawn(|| {
-        
-        upload_media();
-        
-    // });
-
-    // let thread_2 = thread::spawn(|| {
-        
-        related_post_details();
-    // });
-    // thread_1.join().expect("Thread one panicked")
+fn wash_dishes(){
+// takes 20 minutes
+println!("Started washing dishes");
+thread::sleep(Duration::from_millis(2000));
+println!("Finished washing dishes");
 }
 
-fn main() {
-    let start = Instant::now();
-    add_post();
-    let elapsed = start.elapsed();
-    println!("Time elapsed {:?}", elapsed);
+fn main(){
+    clean_room();
+    make_tea();
+    wash_dishes();
 }
